2bf3a074f276285fd3930305636beaec101eee67
André Bargull <andre.bargull@gmail.com>
2015-06-10 22:42:35 +0200

    Remove onlyStrict flag in test/built-ins

--- test/built-ins/Function/15.3.2.1-11-3-s.js
+++ test/built-ins/Function/15.3.2.1-11-3-s.js
@@ -9,13 +9,10 @@ es5id: 15.3.2.1-11-3-s
 description: >
     Function constructor having a formal parameter named 'eval' throws
     SyntaxError if function body is strict mode
-flags: [onlyStrict]
 includes: [runTestCase.js]
 ---*/
 
 function testcase() {
-  
-
   try {
     Function('eval', '"use strict";');
 	return false;
